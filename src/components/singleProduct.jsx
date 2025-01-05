54dbf02ebbe1754a8b67e452cbcc17bb3251fc13
jay-singer <manishimwemugishaj40@gmail.com>
2025-01-05 21:19:10 +0200

    update

--- src/components/singleProduct.jsx
+++ src/components/singleProduct.jsx
@@ -7,7 +7,7 @@ const SingleProduct = ({ items }) => {
         borderTopRightRadius: "8px",
         borderTopLeftRadius: "8px",
       }}
-      className=" mt-3 shadow-sm md:w-[235px]  md:mt-0 xl:w-[250px] sm:w-full md:shadow-none  shadow-gray-400 flex flex-col   space-y-4 overflow-hidden  text-center "
+      className=" mt-3 shadow-sm md:w-full lg:w-[300px] border border-black  md:mt-0 xl:w-[250px] sm:w-full md:shadow-none  shadow-gray-400 flex flex-col   space-y-4 overflow-hidden  text-center "
     >
       <img src={items.pic} alt={items.ProName} />
       <h3 className="whitespace-nowrap w-full hit-fit object-cover">
